cebc4852a5c54aa7e6d32c65511cccf99935c2c9
Richard Vade <ivmidable@gmail.com>
2022-08-24 13:26:23 -0400

    update tests ts

--- node/0_test.ts
+++ node/0_test.ts
@@ -35,24 +35,41 @@ describe("Cosmwasm Template Tests", () => {
         }
     }).timeout(10000);
 
+    xit("Send Testnet Tokens", async() => {
+
+    }).timeout(50000);
 
     //same as
     //junod tx wasm store artifacts/cw1_whitelist.wasm --from wallet --node https://rpc.uni.juno.deuslabs.fi --chain_id=uni-3 --gas-price=0.025ujunox --gas auto
-    it("Upload code to testnet", async () => {
+    xit("Upload code to testnet", async () => {
         let gas = GasPrice.fromString("0.025ujunox");
         let wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, { prefix: 'juno' });
         let client = await SigningCosmWasmClient.connectWithSigner(rpcEndpoint, wallet, {gasPrice:gas});
         let res = await client.upload("juno10c3slrqx3369mfsr9670au22zvq082jaej8ve4", whitelist_wasm, "auto");
         //calculateFee()
-        console.log(res);
+        //console.log(JSON.stringify(res.logs[0].events));
     }).timeout(50000);
 
-    it("Instantiate code on testnet", async() => {
+    xit("Instantiate code on testnet", async() => {
         let gas = GasPrice.fromString("0.025ujunox");
         let wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, { prefix: 'juno' });
         let client = await SigningCosmWasmClient.connectWithSigner(rpcEndpoint, wallet, {gasPrice:gas});
-        let res = await client.instantiate("juno10c3slrqx3369mfsr9670au22zvq082jaej8ve4", 3019, {}, "whitelist", "auto");
+        let res = await client.instantiate("juno10c3slrqx3369mfsr9670au22zvq082jaej8ve4", 3019, {admins:["juno10c3slrqx3369mfsr9670au22zvq082jaej8ve4"], mutable:false}, "whitelist", "auto");
+        console.log(res);
+        ///client.execute("juno10c3slrqx3369mfsr9670au22zvq082jaej8ve4", "", )
     }).timeout(20000);
+
+    xit("Instantiate NFT contract", async() => {
+
+    }).timeout(50000);
+
+    xit("Execute a mint on testnet", async() => {
+        let gas = GasPrice.fromString("0.025ujunox");
+        let wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, { prefix: 'juno' });
+        let client = await SigningCosmWasmClient.connectWithSigner(rpcEndpoint, wallet, {gasPrice:gas});
+        let res = await client.execute("juno10c3slrqx3369mfsr9670au22zvq082jaej8ve4", "juno10c3slrqx3369mfsr9670au22zvq082jaej8ve4",
+            {}, "auto");
+    });
 
 
 
